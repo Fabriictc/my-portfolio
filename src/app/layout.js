ae4ef8a1f9882af8c09096b5deced7b265cf6ef9
Emmanuel Amanga <emmanuelamanga2013@gmail.com>
2025-09-22 15:30:42 +0300

    updated speed insights

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,6 @@
 import Footer from "./components/Footer";
 import Header from "./components/Header";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
@@ -31,6 +32,7 @@ export default function RootLayout({ children }) {
                     <Header />
                     <main className="max-w-6xl mx-auto px-6 py-12">
                         {children}
+                        <SpeedInsights/>
                     </main>
                     <Footer />
                 </div>
